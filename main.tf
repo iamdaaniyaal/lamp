132a47bbb5cc8d3da7bf69229b018baef1ee06b7
Puneeth Irvichetty <puneeth2011@gmail.com>
2019-10-14 17:25:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 // Configure the Google Cloud provider
 provider "google" {
-  credentials = "${file("${var.credentials}")}"
   project     = "${var.gcp_project}"
   region      = "${var.region}"
 }
