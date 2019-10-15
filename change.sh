14f3967c3001906d02d92fd432b3f7e90f3b082e
repakaanjan <40664358+repakaanjan@users.noreply.github.com>
2019-10-15 14:24:01 +0530

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,6 +1,5 @@
 sudo sed -i 's/target_machine/'$machine_type'/' terraform.tfvars
-echo 'export time=$(date +"%m%d%y%H%M%S")' >> ~/.bash_profile
-source ~/.bash_profile
+export time=$(date +"%m%d%y%H%M%S")
 sudo sed -i 's/timestamp/'$time'/' terraform.tfvars
 sudo sed -i 's/vmname/'$stack_name'/' terraform.tfvars
 sudo sed -i 's/stacked/'$stack'/' terraform.tfvars
