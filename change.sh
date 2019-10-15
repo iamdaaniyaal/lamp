6c6b2179585c77dfc67f5b553cae82e5ff5827dc
Puneeth Irvichetty <puneeth2011@gmail.com>
2019-10-15 12:08:48 +0530

    Create change.sh

---
+++ change.sh
@@ -0,0 +1,6 @@
+sudo sed -i 's/target_machine/'$machine_type'/' terraform.tfvars
+echo 'export time=$(date +"%m%d%y%H%M%S")' >> ~/.bash_profile
+source ~/.bash_profile
+sudo sed -i 's/timestamp/'$time'/' terraform.tfvars
+sudo sed -i 's/vmname/'$stack_name'/' terraform.tfvars
+sudo sed -i 's/stacked/'$stack'/' terraform.tfvars
